292f786b4507a9fde70522dd63b4cda11457a4a3
Alan Yao <alaney2@illinois.edu>
2023-11-04 19:31:52 -0500

    focal

--- get_focal.py
+++ get_focal.py
@@ -2,31 +2,28 @@ import math
 import json
 
 
-# camera_angle_x_radians = 0.6911112070083618
-# camera_angle_y_radians = 0.4710899591445923
-# image_width_pixels = 1920
-# image_height_pixels = 1080
+for i in range(3, 41):
 
-# f_x_pixels = image_width_pixels / (2 * math.tan(camera_angle_x_radians / 2))
-# f_y_pixels = image_height_pixels / (2 * math.tan(camera_angle_y_radians / 2))
+    path = f'./plane_dataset/plane{i}'
 
-# focal_length = (f_x_pixels / image_width_pixels + f_y_pixels / image_height_pixels) / 2
+    with open(f'{path}/transforms_train.json', 'r') as file:
+        parsed_data = json.load(file)
 
-# print(f"Computed focal length: {focal_length}")
+    camera_angle_x_radians = parsed_data["camera_angle_x"]
+    camera_angle_y_radians = parsed_data["camera_angle_y"]
+    image_width_pixels = parsed_data["w"]
+    image_height_pixels = parsed_data["h"]
 
-path = './plane_dataset/plane1'
+    f_x_pixels = image_width_pixels / (2 * math.tan(camera_angle_x_radians / 2))
+    f_y_pixels = image_height_pixels / (2 * math.tan(camera_angle_y_radians / 2))
 
-with open(f'{path}/transforms_train.json', 'r') as file:
-    parsed_data = json.load(file)
+    focal_length = (f_x_pixels / image_width_pixels + f_y_pixels / image_height_pixels) / 2
 
-camera_angle_x_radians = parsed_data["camera_angle_x"]
-camera_angle_y_radians = parsed_data["camera_angle_y"]
-image_width_pixels = parsed_data["w"]
-image_height_pixels = parsed_data["h"]
+    print(f"Computed focal length: {focal_length}")
 
-f_x_pixels = image_width_pixels / (2 * math.tan(camera_angle_x_radians / 2))
-f_y_pixels = image_height_pixels / (2 * math.tan(camera_angle_y_radians / 2))
+    frames_data = parsed_data['frames']
 
-focal_length = (f_x_pixels / image_width_pixels + f_y_pixels / image_height_pixels) / 2
+    new_data = {'focal': focal_length, 'frames': frames_data}
 
-print(f"Computed focal length: {focal_length}")
+    with open(f'{path}/transforms_train.json', 'w') as file:
+        json.dump(new_data, file, indent=4)
--- train_test_validate.py
+++ train_test_validate.py
@@ -1,28 +1,30 @@
 import json
 from random import shuffle
 
-path = './plane_dataset/plane1'
+for i in range(2, 41):
 
-with open(f'{path}/transforms_train.json', 'r') as file:
-    full_data = json.load(file)
+    path = f'./plane_dataset/plane{i}'
 
-frames = full_data['frames']
-shuffle(frames)
+    with open(f'{path}/transforms_train.json', 'r') as file:
+        full_data = json.load(file)
 
-train_frames = frames[:60]
-val_frames = frames[60:80]
-test_frames = frames[80:]
+    frames = full_data['frames']
+    shuffle(frames)
 
-train_frames_sorted = sorted(train_frames, key=lambda x: x['file_path'])
-val_frames_sorted = sorted(val_frames, key=lambda x: x['file_path'])
-test_frames_sorted = sorted(test_frames, key=lambda x: x['file_path'])
+    train_frames = frames[:60]
+    val_frames = frames[60:80]
+    test_frames = frames[80:]
+
+    train_frames_sorted = sorted(train_frames, key=lambda x: x['file_path'])
+    val_frames_sorted = sorted(val_frames, key=lambda x: x['file_path'])
+    test_frames_sorted = sorted(test_frames, key=lambda x: x['file_path'])
 
 
-with open(f'{path}/transforms_train.json', 'w') as file:
-    json.dump({'focal': full_data['focal'], 'frames': train_frames_sorted}, file, indent=4)
+    with open(f'{path}/transforms_train.json', 'w') as file:
+        json.dump({'focal': full_data['focal'], 'frames': train_frames_sorted}, file, indent=4)
 
-with open(f'{path}/transforms_val.json', 'w') as file:
-    json.dump({'focal': full_data['focal'], 'frames': val_frames_sorted}, file, indent=4)
+    with open(f'{path}/transforms_val.json', 'w') as file:
+        json.dump({'focal': full_data['focal'], 'frames': val_frames_sorted}, file, indent=4)
 
-with open(f'{path}/transforms_test.json', 'w') as file:
-    json.dump({'focal': full_data['focal'], 'frames': test_frames_sorted}, file, indent=4)
+    with open(f'{path}/transforms_test.json', 'w') as file:
+        json.dump({'focal': full_data['focal'], 'frames': test_frames_sorted}, file, indent=4)
